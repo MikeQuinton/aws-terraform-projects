78ce0b1053892302e384cd1cb14df6d534015d3a
Michael Quinton <32737081+MikeQuinton@users.noreply.github.com>
2024-03-13 09:26:50 +0000

    aws-vpc upload

---
+++ aws-vpc/provider.tf
@@ -0,0 +1,14 @@
+# Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
